29f365724c7d1960c78940bbbf4564449210d7af
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-06-20 16:55:14 +0200

    library project project setup

---
+++ Library/app.js
@@ -0,0 +1,15 @@
+// start with given code
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+// also need to start
+
+//first need to start with a default object with name, author, nb of page, read or not
