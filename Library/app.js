cfdf020df2a9aef8135fcd7e9c97aaa52758dd2b
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-06-20 16:55:49 +0200

    writting need

--- Library/app.js
+++ Library/app.js
@@ -13,3 +13,6 @@ function addBookToLibrary() {
 // also need to start
 
 //first need to start with a default object with name, author, nb of page, read or not
+
+// they want to build with a button to delete, a button to mark read
+//they want a little form to add info
