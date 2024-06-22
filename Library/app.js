f3d31c9807e1034662b259d9e8164d417d7abf9c
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-06-22 16:37:40 +0200

    basic input method

--- Library/app.js
+++ Library/app.js
@@ -2,17 +2,71 @@
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(name, author, pageQuantity, readingStatus = false) {
+  
+  this.name = name;
+  this.author = author;
+  this.pageQuantity = pageQuantity;
+  this.readingStatus = readingStatus;
+  // lets declare new book are unread by default
+
+  let book = {
+    name: name,
+    author: author,
+    pageQuantity: pageQuantity,
+    readingStatus: readingStatus,
+  }
+  
+  return book
 }
 
-function addBookToLibrary() {
-  // do stuff here
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
 
-// also need to start
+//add book with msg
+let button = document.getElementById("button");
+button.addEventListener("click", ()=> {
+  console.log("button clicked");
+  console.log(trialBook);
 
-//first need to start with a default object with name, author, nb of page, read or not
+  //promtp mode
+  var name = prompt("Name of the book");
+  var author = prompt("Name of the author");
+  var pageQuantity = prompt("number of pages");
+  var readingStatus = confirm("Did you read it ?")
 
-// they want to build with a button to delete, a button to mark read
-//they want a little form to add info
+  let promptBook = Book(name, author, pageQuantity, readingStatus);
+  addBookToLibrary(promptBook);
+  console.log(myLibrary);
+                      
+}
+)
+
+
+
+// display library in html using the list
+function DisplayLibrary() {
+  let list = document.getElementById("list");
+
+  myLibrary.forEach(book => {
+    console.log("hot");
+    const bookLine = document.createElement("li");
+
+    let status = if(book.status == true){"read"}else{"not read"};
+      
+
+    bookLine.innerHTML =
+      "
+      <p><b>${book.name}</b>, by ${book.author}, ${book.pageQuantity}, ${status} </p>
+      "
+  })
+
+  
+}
+
+
+//function to change status
+
+//function to delete
