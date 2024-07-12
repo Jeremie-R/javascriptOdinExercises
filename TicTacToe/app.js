b033ae648f879f0b37c62d90a055422201f4e83d
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2024-07-12 16:07:05 +0200

    started tic tac toe

---
+++ TicTacToe/app.js
@@ -0,0 +1,14 @@
+// define player
+function player (symbol) {
+    const symbol = symbol;
+}
+
+
+// create a system for the game to run / test in console
+const game = ()=> {
+    let board = new Array[9];
+
+}
+
+
+// build the game ui
